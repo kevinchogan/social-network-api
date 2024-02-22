30f94a320aac0bc5b77928c29f5e9ea551faa6a1
Kevin Hogan <kchogan@pacbell.net>
2024-02-21 18:47:29 -0800

    formatting

--- models/User.js
+++ models/User.js
@@ -34,12 +34,10 @@ const userSchema = new Schema(
   }
 );
 
-userSchema
-  .virtual('friendCount')
-  .get(function () {
-    return this.friends.length;
-  });
+userSchema.virtual("friendCount").get(function () {
+  return this.friends.length;
+});
 
-const User = model('User', userSchema);
+const User = model("User", userSchema);
 
 module.exports = User;
